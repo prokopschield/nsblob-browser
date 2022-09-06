f832dd14c95adf20ccaaab02f6b29c40ee2564bf
Prokop <prokop.schield@protonmail.com>
2022-09-06 11:26:16 +0200

    feat(src/index.ts): define module as window

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,3 @@
-import nsblob from 'nsblob';
-import Buffer from 'fix-buffer-in-browser';
-
 if (typeof window === 'undefined') {
 	Object.assign(global, {
 		window: global,
@@ -12,6 +9,15 @@ if (typeof global === 'undefined') {
 		global: window,
 	});
 }
+
+if (typeof module === 'undefined') {
+	Object.assign(window, {
+		module: window,
+	});
+}
+
+import nsblob from 'nsblob';
+import Buffer from 'fix-buffer-in-browser';
 
 if (!('Buffer' in window)) {
 	Object.assign(window, Buffer);
